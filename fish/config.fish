2d51f2e855d82b93faf7ba85acacd6de7c4e86e4
merzen-sh <yuthana.wareesri.2001@gmail.com>
2026-06-04 01:49:41 +0700

    update fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,24 +1,11 @@
 source (dirname (status filename))/functions/alias.fish
 source (dirname (status filename))/functions/zoxide.fish
 
-# opencode
-fish_add_path $HOME/.opencode/bin
-
 set -U fish_greeting ""
 
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
-
-# pnpm
-set -gx PNPM_HOME "$HOME/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
-
-# npm
-set -gx PATH (npm config get prefix)/bin $PATH
 
 #oh my posh
 oh-my-posh init fish --config $HOME/dotfiles/mark.omp.json | source
@@ -29,8 +16,3 @@ fastfetch
 
 set -g fish_ambiguous_width 1
 set -g fish_emoji_width 2
-
-# zoxide init fish | source  # disabled: uses 'alias' incompatible with this fish version
-
-# Added by trek installer
-fish_add_path "/home/mark/.trek/bin"
