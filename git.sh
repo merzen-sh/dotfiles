5d0095bae691c61d38dad79680b64bdc15fb074d
merzen-sh <yuthana.wareesri.2001@gmail.com>
2026-06-10 13:28:56 +0700

    chore: update nvim plugins

--- git.sh
+++ git.sh
@@ -1,3 +1,6 @@
 git config --global pull.rebase true
 git config --global push.autoSetupRemote true
 git config --global init.defaultBranch main
+
+git config --global user.name "merzen-sh"
+git config --global user.email "yuthana.wareesri.2001@gmail.com"
